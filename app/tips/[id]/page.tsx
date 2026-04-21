0202dd97e86b0b6d2362f78c4520e199a71909df
isaiahremi-commits <isaiah.remi@thesuitch.com>
2026-04-21 10:42:34 -0600

    feat: auto-fill team from schedule, clearer commission labeling, fixed back button

--- app/tips/[id]/page.tsx
+++ app/tips/[id]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useCallback, useEffect, useMemo, useState } from "react";
-import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 
 type Employee = { id: string; name: string };
